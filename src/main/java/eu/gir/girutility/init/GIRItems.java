af0e344c652fa34237c24b6c4795917c576d225d
Philipp <philippdal@gmail.com>
2022-07-21 20:29:57 +0200

    feat: better item registration

--- src/main/java/eu/gir/girutility/init/GIRItems.java
+++ src/main/java/eu/gir/girutility/init/GIRItems.java
@@ -1,42 +1,51 @@
 package eu.gir.girutility.init;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 
 import eu.gir.girutility.GirutilityMain;
 import eu.gir.girutility.items.ItemReflectiveArmor;
 import eu.gir.girutility.items.SlabItem;
-import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemArmor.ArmorMaterial;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class GIRItems {
 
-    public static final ArmorMaterial reflectiveArmorMaterial = EnumHelper
-            .addArmorMaterial("reflective", GirutilityMain.MODID + ":reflective", 1000, new int[] {
-                    1, 1, 1, 1
-            }, 30, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0F);
-    public static final ItemReflectiveArmor reflectiveChestplate = new ItemReflectiveArmor(
-            reflectiveArmorMaterial, 1, EntityEquipmentSlot.CHEST);
-    public static final SlabItem slabItem = new SlabItem(GIRBlocks.SLAB1_HALF, GIRBlocks.SLAB1_HALF, GIRBlocks.SLAB1_DOUBLE);
+    public static final ItemReflectiveArmor reflective_Chestplate = new ItemReflectiveArmor(
+            ItemReflectiveArmor.reflectiveArmorMaterial, 1, EntityEquipmentSlot.CHEST);
+    public static final SlabItem slabItem = new SlabItem(GIRBlocks.SLAB1_HALF, GIRBlocks.SLAB1_HALF,
+            GIRBlocks.SLAB1_DOUBLE);
 
     public static ArrayList<Item> itemsToRegister = new ArrayList<>();
 
     public static void init() {
-        setName(reflectiveChestplate, "reflective_chestplate");
-        setName(slabItem, "slab_item");
+        Field[] fields = GIRItems.class.getFields();
+        for (Field field : fields) {
+            int modifiers = field.getModifiers();
+            if (Modifier.isStatic(modifiers) && Modifier.isFinal(modifiers)
+                    && Modifier.isPublic(modifiers)) {
+                String name = field.getName().toLowerCase();
+                try {
+                    Item item = (Item) field.get(null);
+                    item.setRegistryName(new ResourceLocation(GirutilityMain.MODID, name));
+                    item.setUnlocalizedName(name);
+                    itemsToRegister.add(item);
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     @SubscribeEvent
     public static void registerItem(RegistryEvent.Register<Item> event) {
         IForgeRegistry<Item> registry = event.getRegistry();
-        registry.register(reflectiveChestplate);
-        registry.register(slabItem);
+        itemsToRegister.forEach(registry::register);
     }
 
     public static void setName(Item item, String name) {
--- src/main/java/eu/gir/girutility/items/ItemReflectiveArmor.java
+++ src/main/java/eu/gir/girutility/items/ItemReflectiveArmor.java
@@ -1,8 +1,11 @@
 package eu.gir.girutility.items;
 
+import eu.gir.girutility.GirutilityMain;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
+import net.minecraftforge.common.util.EnumHelper;
 
 public class ItemReflectiveArmor extends ItemArmor {
 
@@ -11,5 +14,10 @@ public class ItemReflectiveArmor extends ItemArmor {
         super(materialIn, renderIndexIn, equipmentSlotIn);
         setCreativeTab(CreativeTabs.COMBAT);
     }
+    
+    public static final ArmorMaterial reflectiveArmorMaterial = EnumHelper
+            .addArmorMaterial("reflective", GirutilityMain.MODID + ":reflective", 1000, new int[] {
+                    1, 1, 1, 1
+            }, 30, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0F);
 
 }
