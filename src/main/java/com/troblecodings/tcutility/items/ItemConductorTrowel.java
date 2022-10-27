29fc5790d5f0cefc1f510110466a7b05ba0f9a0e
Philipp <philippdal@gmail.com>
2022-10-27 19:48:16 +0200

    feat: added conductor trowel

---
+++ src/main/java/com/troblecodings/tcutility/items/ItemConductorTrowel.java
@@ -0,0 +1,11 @@
+package com.troblecodings.tcutility.items;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class ItemConductorTrowel extends Item {
+
+    public ItemConductorTrowel() {
+        setCreativeTab(CreativeTabs.COMBAT);
+    }
+}
